f3291a8f759b0eaa3794b248ac0b360dd5cb4715
Lauro Sollero <lauro.sollero@zoly.com.br>
2021-01-21 20:03:59 -0300

    .

--- lp-enfermagem/app/main.js
+++ lp-enfermagem/app/main.js
@@ -1,6 +1,7 @@
 define([
   "jquery",
   "js/lib/popper.min",
+  "bootstrap",
   "select2",
   "js/lib/global-scripts",
 ], function ($) {
--- lp-enfermagem/js/config-21-01.js
+++ lp-enfermagem/js/config-21-01.js
@@ -5,5 +5,6 @@ var require = {
     app: "app",
     jquery: "//code.jquery.com/jquery-3.5.1.min",
     select2: "//cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min",
+    bootstrap: "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min",
   },
 };
