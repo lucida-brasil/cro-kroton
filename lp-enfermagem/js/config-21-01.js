9dbbc2007c840e572de4b0ef8d61eb8d546d07d3
Lauro Sollero <lauro.sollero@zoly.com.br>
2021-01-21 20:08:14 -0300

    .

--- lp-enfermagem/js/config-21-01.js
+++ lp-enfermagem/js/config-21-01.js
@@ -2,6 +2,7 @@ var require = {
   baseUrl:
     "https://cdn.statically.io/gh/lucida-brasil/cro-kroton/main/lp-enfermagem",
   paths: {
+    main: "js/app",
     app: "app",
     jquery: "//code.jquery.com/jquery-3.5.1.min",
     select2: "//cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min",
