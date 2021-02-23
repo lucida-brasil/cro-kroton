063eda649f51ec35f09522aac393a64a66f38fd8
Lauro Sollero <lauro.sollero@zoly.com.br>
2021-02-23 13:48:35 -0300

    hotfix duplo cta enfermagem

--- lp-enfermagem/js/scripts-02-08b.js
+++ lp-enfermagem/js/scripts-02-08b.js
@@ -318,9 +318,13 @@ var headerTeste = {
       cont.classList.add("second-level");
       cont.appendChild(this.acompanhe);
       cont.appendChild(this.but);
-      document.querySelector(".anhanguera_ > .second-menu").appendChild(cont);
+      if (document.querySelectorAll("#second-menu__nav .cta-btn").length === 0) {
+        document.querySelector(".anhanguera_ > .second-menu").appendChild(cont);
+      }
     } else {
-      document.querySelector("#second-menu__nav").appendChild(this.but);
+      if (document.querySelectorAll("#second-menu__nav .cta-btn").length === 0) {
+        document.querySelector("#second-menu__nav").appendChild(this.but);
+      }
     }
     this.bind();
   },
